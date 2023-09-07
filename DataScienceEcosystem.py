e7217c649a465af41e2a2a0bd49852bf08745d73
aleveden <143224104+aleveden@users.noreply.github.com>
2023-09-07 09:14:05 -0500

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# >In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# <b> Objectives:</b>
+# 
+# In this final assignment, you will:
+# 
+# - Create a Jupyter Notebook
+# 
+# - Insert code and markdown cells
+# 
+# - Share your notebook through GitHub
+
+# Some of the popular languages that Data Scientists use are:
+# 1. TensorFlow
+# 2. NumPy
+# 3. SciPy
+
+# |Data Science Tools|
+# |:----------------:|
+# |RStudio|
+# |Apache Spark|
+# |TensorFlow|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[12]:
+
+
+(3*4)+5
+#his will convert 200 minutes to hours by diving by 60
+
+
+# ## Author
+# Alejandra Vélez
